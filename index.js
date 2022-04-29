210b657d63ed43b2ffe7d0513d6b0f1fffc60b88
abrakib942 <abrakib942@gmail.com>
2022-04-29 17:21:32 +0600

    get the api from dataase

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,33 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://groceryItem:abrakib1234@cluster0.axi3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const inventoryCollection = client.db("groceryItem").collection("item");
+
+    //  Inventory API
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("warehouse management running");
